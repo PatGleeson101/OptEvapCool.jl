a083aeb78b0559af1de193b536fc3aa767bb2b91
Patrick Gleeson <patrickgleeson101@gmail.com>
2021-08-13 18:04:59 +1000

    Correct energy and distance scales

--- src/measure.jl
+++ src/measure.jl
@@ -6,18 +6,20 @@ using Statistics: mean
 
 const kB = 1.38064852e-23 #Boltzmann constant
 
-# Compute total translational kinetic energy
-function total_kinetic_energy(velocities, m)
-    return 0.5 * m * sum(velocities .* velocities)
+# Compute massic translational kinetic energy
+function massic_kinetic_energy(velocities)
+    # The extra 3 is to account for the fact that the velocities
+    # array has 3xN elements, not just N.
+    return 0.5 * 3 * mean(velocities .* velocities)
 end
 
-function total_potential_energy(positions, potential, t, m)
-    return m * sum(potential(positions, t))
+function massic_potential_energy(positions, potential, t)
+    return mean(potential(positions, t))
 end
 
 function avg_temperature(velocities, m)
-    K = total_kinetic_energy(velocities, m)
-    return 2 * K / (3 * size(velocities, 2) * kB)
+    avg_ke = m * massic_kinetic_energy(velocities)
+    return 2 * avg_ke / (3 * kB)
 end
 
 # Generate a function to record temperature and time in the provided arrays
@@ -28,10 +30,10 @@ function record_temperature(temp_array, time_array, m)
     end
 end
 
-function record_total_energy(energy_array, time_array, potential, m)
+function record_massic_energy(energy_array, time_array, potential)
     return function(positions, velocities, t)
-        ke = total_kinetic_energy(velocities, m)
-        pe = total_potential_energy(positions, potential, t, m)
+        ke = massic_kinetic_energy(velocities)
+        pe = massic_potential_energy(positions, potential, t)
         push!(energy_array, ke + pe)
         push!(time_array, t)
     end
@@ -39,15 +41,17 @@ end
 
 # Key function: records several quantities.
 function record_all(time_array, ke_array, pe_array, dist_array,
-                    temp_array, potential, start_positions, m)
+                    temp_array, potential, start_positions, m,
+                    test_particle_array)
     initial_positions = copy(start_positions)
     return function(positions, velocities, t)
         push!(time_array, t)
-        push!(ke_array, total_kinetic_energy(velocities, m))
-        push!(pe_array, total_potential_energy(positions, potential, t, m))
+        push!(ke_array, massic_kinetic_energy(velocities))
+        push!(pe_array, massic_potential_energy(positions, potential, t))
         displacements = positions - initial_positions
         avg_distance = mean(sqrt.(sum(displacements .^2, dims = 1)))
         push!(dist_array, avg_distance)
         push!(temp_array, avg_temperature(velocities, m))
+        push!(test_particle_array, positions[:,1])
     end
 end
